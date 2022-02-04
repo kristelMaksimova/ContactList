24d5ff6bf7716a052741d3bb32b4141d7f9491ca
Maksimova Kristel <idmaksimovak@gmail.com>
2022-02-04 21:52:09 +0300

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Kristi on 04.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
